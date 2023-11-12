675e7cb889503f2545617e1192cee2ec818cfd11
katayama8000 <tattu.0310@gmail.com>
2023-11-12 21:57:39 +0900

    chore

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 - https://graphql.org/graphql-js/running-an-express-graphql-server/
 - https://www.wakuwakubank.com/posts/641-nodejs-graphql/
 - https://zenn.dev/sungvalley/articles/4de76c12826709
+- https://zenn.dev/harusame0616/articles/170b6ae38de086 // <- この続きをやる
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,23 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 
-// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String,
-    name: String,
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
-// The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
-  name: () => {
-    return 'My name is Khoa';
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
   },
 };
 
@@ -24,10 +26,11 @@ const app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
+    schema,
     rootValue: root,
     graphiql: true,
   })
 );
+
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
